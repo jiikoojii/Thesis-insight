0c0c3a6f7ebb33df273140d9b770ec34890adce5
jiikoojii <52273200+jiikoojii@users.noreply.github.com>
2020-09-16 18:55:17 +0300

    Update pull-from-OTX.sh

--- pull-from-OTX.sh
+++ pull-from-OTX.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-taxii-poll --path https://otx.alienvault.com/taxii/poll --collection user_AlienVault --username 0f9645cdc183e55ab622272962ff596f9663c0815bc92eb2807d7ba565967e43 --password foo --begin 2020-09-01 --dest-dir /home/janij/testi/
+taxii-poll --path https://otx.alienvault.com/taxii/poll --collection user_AlienVault --username 'username' --password foo --begin 2020-09-01 --dest-dir /home/janij/testi/
