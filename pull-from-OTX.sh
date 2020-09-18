fdf7932c30fa4e9591a1423b35d2d89c720bb4fa
jiikoojii <52273200+jiikoojii@users.noreply.github.com>
2020-09-18 17:35:28 +0300

    Added a comment

--- pull-from-OTX.sh
+++ pull-from-OTX.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#--dest-dir is a must, else it will print it to screen
 taxii-poll --path https://otx.alienvault.com/taxii/poll --collection user_AlienVault --username 'username' --password foo --begin 2020-09-01 --dest-dir /path/to/somewehere/
