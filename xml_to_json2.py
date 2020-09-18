b368ac8749bdb7fd4af98db287ec55667c7ed681
jiikoojii <52273200+jiikoojii@users.noreply.github.com>
2020-09-18 13:44:56 +0300

    Add files via upload
    Script to convert XML files to JSON, meant to accompany pull-from-otx.sh.

---
+++ xml_to_json2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+import xmltodict
+import json
+import sys
+
+xml_file = sys.argv[1]
+#user_named = sys.argv[2]
+tree = ET.parse(xml_file)
+xml_data = tree.getroot()
+
+xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
+
+data_dict = dict(xmltodict.parse(xmlstr))
+
+json_file = xml_file + ".json"
+with open(json_file, 'w+') as json_file:
+    json.dump(data_dict, json_file, indent=4, sort_keys=True)
